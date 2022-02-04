62b7c62702f14be619f949b6a61c1b80de74948c
Oskar Rough <oskar@rough.dk>
2022-02-04 17:38:48 +0100

    More

--- client/index.js
+++ client/index.js
@@ -1,5 +1,15 @@
-// Create WebSocket connection.
-const socket = new WebSocket('ws://localhost:8080')
+// Create a WebRTC connection
+const config = {iceServes: [{'urls': 'stun:stun.l.google.com:19302'}]}
+const peer = new RTCPeerConnection(config)
+
+// Create WebSocket connection
+const host = 'localhost'
+const socket = new WebSocket(`ws://${host}:8080`)
+
+// Connection error
+socket.addEventListener('error', function error(err) {
+	console.error(err)
+})
 
 // Connection opened
 socket.addEventListener('open', function (event) {
@@ -7,14 +17,37 @@ socket.addEventListener('open', function (event) {
 })
 
 // Listen for messages
-socket.addEventListener('message', function (event) {
+socket.addEventListener('message', async function (event) {
 	console.log('Message from server: ', event.data)
-	// const msg = JSON.parse(event.data)
-	// console.log(msg)
+
+	if (event.data instanceof Blob) {
+		const reader = new FileReader()
+		reader.addEventListener('load', async () => {
+			const msg = JSON.parse(reader.result)
+			console.log(msg)
+
+			if (msg.type === 'offer') {
+				console.log('creating an offer')
+				const desc = new RTCSessionDescription(msg)
+				peer.setRemoteDescription(desc)
+				console.log('.. and sending an answer')
+				const answer = await peer.createAnswer()
+				await peer.setLocalDescription(answer)
+				socket.send(JSON.stringify(answer))
+			}
+
+			if (msg.type === 'answer') {
+				console.log('setting an answer')
+				const desc = new RTCSessionDescription(msg)
+				await peer.setRemoteDescription(desc)
+			}
+		})
+		reader.readAsText(event.data)
+	}
 })
 
-document.querySelector('form').addEventListener('submit', sendMessage)
 
+document.querySelector('form').addEventListener('submit', sendMessage)
 function sendMessage(event) {
 	event.preventDefault()
 	const data = new FormData(event.target)
@@ -23,9 +56,12 @@ function sendMessage(event) {
 	socket.send(msg)
 }
 
-function appendMessage(msg) {
-	const list = document.querySelector('#list')
-	const li = document.createElement('li')
-	li.innerHTML = msg
-	list.appendChild(li)
+document.querySelector('button#connect').addEventListener('click', makeCall)
+// server creates connection and receives "offer"
+async function makeCall() {
+	console.log('creating and sending offer')
+	const offer = await peer.createOffer()
+	console.log(offer)
+	await peer.setLocalDescription(offer)
+	socket.send(JSON.stringify(offer))
 }
--- server/index.js
+++ server/index.js
@@ -1,21 +1,35 @@
-// import {createServer} from 'https'
-import { WebSocketServer } from 'ws'
+import WebSocket, {WebSocketServer} from 'ws'
 
 const port = 8080
-
-// const server = createServer()
 const wss = new WebSocketServer({port})
 
 wss.on('connection', function connection(ws) {
 	console.log('server running on', port)
 
-	ws.on('message', function message(data) {
+	ws.on('message', function message(data, isBinary) {
 		console.log('received: %s', data)
-		// for (let client of clients) {
-		// 	client.send(msg)
-		// }
+		// if (typeof data === 'string') return
+		// let parsed = data
+		try {
+			let parsed = JSON.parse(data)
+			console.log(parsed)
+			if (parsed.type === 'offer') {
+				for (let client of wss.clients) {
+					if (/*client !== ws && */client.readyState === WebSocket.OPEN) {
+						client.send(data)
+					}
+				}
+				
+			}
+			if (parsed.type === 'answer') {
+				// what now
+			}
+		} catch (err) {
+			// console.error(err)
+		}
+		console.log(data)
 	})
+
 	ws.send('hello from server')
 })
 
-// server.listen(port)
