34967080fec36df52a32aa72af4a26755db175a7
Oskar Rough <oskar@rough.dk>
2022-02-05 13:34:26 +0100

    Checkpoint, can send stuff

--- client/index.js
+++ client/index.js
@@ -6,7 +6,6 @@ window.peer = peer
 
 // Attach a data channel for later.
 const dataChannel = peer.createDataChannel('main')
-
 function handleChannel(channel) {
 	channel.onopen = async function (event) {
 		console.log('data channel open', channel.readyState)
@@ -15,7 +14,8 @@ function handleChannel(channel) {
 		console.log('data channel close', event)
 	}
 	channel.onmessage = async function (event) {
-		console.log('data channel message', event.data)
+		console.log('data channel message')
+		console.log(decode(event.data))
 	}
 }
 
@@ -26,64 +26,52 @@ peer.onconnectionstatechange = function (event) {
 		console.log('peers connected!!')
 	}
 }
-peer.onicecandidate = function ({candidate}) {
-	if (candidate) {
-		console.log('candidate', candidate)
-		socket.send(JSON.stringify({type: 'candidate', candidate}))
+peer.onicecandidate = async function (event) {
+	const state = event.target.iceGatheringState
+	console.log('onicecandidate', state)
+	if (event.target.iceGatheringState == 'complete') {
+		const offer = await peer.createOffer()
+		console.log('Created new offer with ICE candidates')
+		console.log(encode(offer))
 	}
+}
+peer.oniceconnectionstatechange = function (event) {
+	console.log('ICE connection state change' + event.target.iceConnectionState)
 }
 peer.ondatachannel = function ({channel}) {
 	console.log('ondatachannel', channel)
 	handleChannel(channel)
 }
-peer.onmessage = function (event) {
-	console.log('peer message', event)
-}
 
 // Create WebSocket connection
-
-const host = 'localhost'
-const socket = new WebSocket(`ws://${host}:8080`)
-socket.addEventListener('error', function error(err) {
-	document.querySelector('form#websockets button').disabled = true
-	console.error(err)
-})
-socket.addEventListener('open', function (event) {
-	document.querySelector('form#websockets button').disabled = false
-	socket.send('Hello Server, sent from client!')
-})
-socket.addEventListener('message', async function (event) {
-	console.log('Message from socket server: ', event.data)
-	if (event.data instanceof Blob) {
-		const msg = processBlob(event.data)
-		console.log(msg.type, msg)
-
-		if (msg.type === 'offer') {
-			console.log('creating an offer')
-			const desc = new RTCSessionDescription(msg)
-			peer.setRemoteDescription(desc)
-			console.log('.. and sending an answer')
-			const answer = await peer.createAnswer()
-			await peer.setLocalDescription(answer)
-			socket.send(JSON.stringify(answer))
-		}
-
-		if (msg.type === 'answer') {
-			console.log('setting an answer')
-			const desc = new RTCSessionDescription(msg)
-			await peer.setRemoteDescription(desc)
-		}
-
-		if (msg.type === 'candidate') {
-			try {
-				console.log('adding candidate to peer')
-				peer.addIceCandidate(msg.candiate)
-			} catch (err) {
-				console.error('Error adding ice candidate')
+function setupWebSockets() {
+	const host = 'localhost'
+	const socket = new WebSocket(`ws://${host}:8080`)
+	socket.addEventListener('error', function error(err) {
+		document.querySelector('form#websockets button').disabled = true
+		console.error(err)
+	})
+	socket.addEventListener('open', function (event) {
+		document.querySelector('form#websockets button').disabled = false
+		socket.send('Hello Server, sent from client!')
+	})
+	socket.addEventListener('message', async function (event) {
+		console.log('Message from socket server: ', event.data)
+		if (event.data instanceof Blob) {
+			const msg = processBlob(event.data)
+			console.log(msg.type, msg)
+			if (msg.type === 'candidate') {
+				try {
+					console.log('adding candidate to peer')
+					peer.addIceCandidate(msg.candiate)
+				} catch (err) {
+					console.error('Error adding ice candidate')
+				}
 			}
 		}
-	}
-})
+	})
+	return socket
+}
 
 // Form to send message via websockets.
 document.querySelector('form#websockets').addEventListener('submit', (event) => {
@@ -100,7 +88,7 @@ document.querySelector('form#webrtc').addEventListener('submit', (event) => {
 	const data = new FormData(event.target)
 	const msg = data.get('message')
 	console.log('sending via webrtc', msg)
-	dataChannel.send({custom: 'yolo', msg})
+	dataChannel.send(encode({custom: 'yolo', msg}))
 })
 
 document.querySelector('form#webrtc-offer').addEventListener('submit', async (event) => {
@@ -121,6 +109,12 @@ document.querySelector('form#webrtc-offer').addEventListener('submit', async (ev
 		const desc = new RTCSessionDescription(msg)
 		await peer.setRemoteDescription(desc)
 		console.log('accepted answer')
+	}
+
+	if (msg.type === 'candidate') {
+		console.log('received candidate, adding to local peer', msg.candidate)
+		peer.addIceCandidate(msg.candidate)
+		// socket.send(JSON.stringify({type: 'candidate', candidate}))
 	}
 })
 
