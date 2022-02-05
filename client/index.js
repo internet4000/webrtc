8a2f3dd2c85297a178e25d0212a0b84192ede74f
Oskar Rough <oskar@rough.dk>
2022-02-05 12:55:41 +0100

    Try without server

--- client/index.js
+++ client/index.js
@@ -1,115 +1,91 @@
 // Create a WebRTC connection
-// const config = {iceServes: [{'urls': 'stun:stun.l.google.com:19302'}]}
-const config = {}
+const config = {iceServes: [{urls: 'stun:stun.l.google.com:19302'}]}
 const peer = new RTCPeerConnection(config)
+
+window.peer = peer
+
+// Attach a data channel for later.
 const dataChannel = peer.createDataChannel('main')
 
-dataChannel.onopen = async function (event) {
-	console.log('data channel open', dataChannel.readyState)
-}
-dataChannel.onclose = async function (event) {
-	console.log('data channel close')
-}
-dataChannel.onmessage = async function (event) {
-	console.log('data channel msg', event.data)
+function handleChannel(channel) {
+	channel.onopen = async function (event) {
+		console.log('data channel open', channel.readyState)
+	}
+	channel.onclose = async function (event) {
+		console.log('data channel close', event)
+	}
+	channel.onmessage = async function (event) {
+		console.log('data channel message', event.data)
+	}
 }
 
+// Listen for events on the webrtc peer.
 peer.onconnectionstatechange = function (event) {
 	console.log('peer ocnnection state change', event)
-}
-
-peer.onicecandidate = function (event) {
-	if (event.candidate) {
-		console.log('candidate', event.candidate)
-		socket.send(JSON.stringify({
-			type: 'candidate',
-			candidate: event.candidate,
-		}))
-	}
-}
-
-peer.ondatachannel = function (event) {
-	console.log('ondatachannel')
-	var receiveChannel = event.channel
-	receiveChannel.onmessage = function (event) {
-		console.log('ondatachannel message:', event.data)
-	}
-}
-
-// Listen for connectionstatechange on the local RTCPeerConnection
-peer.addEventListener('connectionstatechange', (event) => {
 	if (peer.connectionState === 'connected') {
 		console.log('peers connected!!')
-		// Peers connected!
 	}
-})
+}
+peer.onicecandidate = function ({candidate}) {
+	if (candidate) {
+		console.log('candidate', candidate)
+		socket.send(JSON.stringify({type: 'candidate', candidate}))
+	}
+}
+peer.ondatachannel = function ({channel}) {
+	console.log('ondatachannel', channel)
+	handleChannel(channel)
+}
+peer.onmessage = function (event) {
+	console.log('peer message', event)
+}
 
 // Create WebSocket connection
+
 const host = 'localhost'
 const socket = new WebSocket(`ws://${host}:8080`)
-
-// Connection error
 socket.addEventListener('error', function error(err) {
+	document.querySelector('form#websockets button').disabled = true
 	console.error(err)
 })
-
-// Connection opened
 socket.addEventListener('open', function (event) {
+	document.querySelector('form#websockets button').disabled = false
 	socket.send('Hello Server, sent from client!')
 })
-
-// Listen for messages
 socket.addEventListener('message', async function (event) {
 	console.log('Message from socket server: ', event.data)
-
 	if (event.data instanceof Blob) {
-		const reader = new FileReader()
-		reader.addEventListener('load', async () => {
-			const msg = JSON.parse(reader.result)
-			console.log(msg.type, msg)
+		const msg = processBlob(event.data)
+		console.log(msg.type, msg)
 
-			if (msg.type === 'offer') {
-				console.log('creating an offer')
-				const desc = new RTCSessionDescription(msg)
-				peer.setRemoteDescription(desc)
-				console.log('.. and sending an answer')
-				const answer = await peer.createAnswer()
-				await peer.setLocalDescription(answer)
-				socket.send(JSON.stringify(answer))
+		if (msg.type === 'offer') {
+			console.log('creating an offer')
+			const desc = new RTCSessionDescription(msg)
+			peer.setRemoteDescription(desc)
+			console.log('.. and sending an answer')
+			const answer = await peer.createAnswer()
+			await peer.setLocalDescription(answer)
+			socket.send(JSON.stringify(answer))
+		}
+
+		if (msg.type === 'answer') {
+			console.log('setting an answer')
+			const desc = new RTCSessionDescription(msg)
+			await peer.setRemoteDescription(desc)
+		}
+
+		if (msg.type === 'candidate') {
+			try {
+				console.log('adding candidate to peer')
+				peer.addIceCandidate(msg.candiate)
+			} catch (err) {
+				console.error('Error adding ice candidate')
 			}
-
-			if (msg.type === 'answer') {
-				console.log('setting an answer')
-				const desc = new RTCSessionDescription(msg)
-				await peer.setRemoteDescription(desc)
-			}
-
-			if (msg.type === 'candidate') {
-				try {
-					console.log('adding candidate to peer')
-					peer.addIceCandidate(msg.candiate)
-				} catch (err) {
-					console.error('Error adding ice candidate')
-				}
-			}
-		})
-
-		// This is just here to trigger the onload
-		reader.readAsText(event.data)
+		}
 	}
 })
 
-// Listen for data channel events
-socket.addEventListener('datachannel', function (event) {
-	const channel = event.channel
-	channel.addEventListener('open', (event) => {
-		console.log('opened data channel')
-	})
-	channel.addEventListener('close', (event) => {
-		console.log('closed data channel')
-	})
-})
-
+// Form to send message via websockets.
 document.querySelector('form#websockets').addEventListener('submit', (event) => {
 	event.preventDefault()
 	const data = new FormData(event.target)
@@ -118,6 +94,7 @@ document.querySelector('form#websockets').addEventListener('submit', (event) => 
 	socket.send(msg)
 })
 
+// Form to send message via WebRTC.
 document.querySelector('form#webrtc').addEventListener('submit', (event) => {
 	event.preventDefault()
 	const data = new FormData(event.target)
@@ -126,12 +103,42 @@ document.querySelector('form#webrtc').addEventListener('submit', (event) => {
 	dataChannel.send({custom: 'yolo', msg})
 })
 
-document.querySelector('button#connect').addEventListener('click', makeCall)
-// server creates connection and receives "offer"
-async function makeCall() {
-	console.log('creating and sending offer')
+document.querySelector('form#webrtc-offer').addEventListener('submit', async (event) => {
+	event.preventDefault()
+	const data = new FormData(event.target)
+	const msg = decode(data.get('message'))
+	console.log(msg)
+	const type = msg.type
+	if (msg.type === 'offer') {
+		const desc = new RTCSessionDescription(msg)
+		peer.setRemoteDescription(desc)
+		const answer = await peer.createAnswer()
+		await peer.setLocalDescription(answer)
+		console.log('accepted offer, here is the answer', encode(answer))
+	}
+
+	if (msg.type === 'answer') {
+		const desc = new RTCSessionDescription(msg)
+		await peer.setRemoteDescription(desc)
+		console.log('accepted answer')
+	}
+})
+
+// Button that creates an offer and sends it
+document.querySelector('button#connect').addEventListener('click', async () => {
 	const offer = await peer.createOffer()
-	console.log(offer)
 	await peer.setLocalDescription(offer)
-	socket.send(JSON.stringify(offer))
+	console.log('creating offer', encode(offer))
+	// socket.send(JSON.stringify(offer))
+})
+
+// Helpers
+function processBlob(blob) {
+	const promise = new Promise()
+	const reader = new FileReader()
+	reader.addEventListener('load', () => resolve(reader.result))
+	reader.readAsText(blob)
+	return promise
 }
+const encode = (data) => btoa(JSON.stringify(data))
+const decode = (data) => JSON.parse(atob(data))
