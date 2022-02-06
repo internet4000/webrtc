0e48e31f7200c2917fd944e5e0b1f923e74f76de
Oskar Rough <oskar@rough.dk>
2022-02-06 11:02:26 +0100

    Look into exchanging ICE candidates

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
-An experiment with webrtc.
+# An experiment with webrtc
 
-To play around you need to:
+## To play around you need to:
 
 1. `npm start` which starts the node server
 2. `npm run client` which open the `client` folder in a local web server
 
-Good resources
+## The dance
+
+1. User1 sends an "offer2"
+2. User2 accepts offer as "remote description"
+3. User2 replies with an answer (also sets it as "local description")
+4. User1 accepts answer
+5. Listen for ICE candidates and exchange them
+
+## Tips
+
+- Candidate gathering starts when you call `setLocalDescription()`
+
+### Good resources
 
 - https://hpbn.co/webrtc/
+- https://codelabs.developers.google.com/codelabs/webrtc-web/#0
--- client/index.js
+++ client/index.js
@@ -1,30 +1,48 @@
 function peerConnection() {
-	// Create a WebRTC connection
-	const config = {iceServes: [{urls: 'stun:stun.l.google.com:19302'}]}
+	const config = {
+		iceServers: [{urls: ['stun:stun.l.google.com:19302', 'stun:global.stun.twilio.com:3478']}],
+	}
 	const peer = new RTCPeerConnection(config)
 
 	// Listen for events on the webrtc peer.
 	peer.onconnectionstatechange = function (event) {
-		console.log('peer ocnnection state change', event)
+		console.log('peer connection state change', event)
 		if (peer.connectionState === 'connected') {
 			console.log('peers connected!!')
 		}
 	}
+	peer.onsignalingstatechange = function (event) {
+		console.log(
+			'peer signalingstatechange',
+			event.target.iceConnectionState,
+			event.target.iceGatheringState,
+			event.target.signalingState
+		)
+	}
 	peer.oniceconnectionstatechange = function (event) {
-		console.log('ICE connection state change' + event.target.iceConnectionState)
+		console.log('peer ice connection state change' + event.target.iceConnectionState)
 	}
 	peer.onicecandidate = async function (event) {
 		const state = event.target.iceGatheringState
-		console.log('onicecandidate', state)
-		if (event.target.iceGatheringState == 'complete') {
+		console.log('peer onicecandidate', state)
+		if (event.candidate) {
+			console.log('candidate to send', event.candidate)
+			// send to other peer
+		} else {
+			console.log('trickle ice done?')
+			console.log(peer.localDescription.type)
 			const offer = await peer.createOffer()
+			// Run this? Not sure. Maybe on peer1 only. setRemoteDescription(offer)
 			console.log('offer (with ICE candidates)', encode(offer))
+		}
+		if (state == 'complete') {
 		}
 	}
 	peer.ondatachannel = function ({channel}) {
 		console.log('ondatachannel', channel)
 		channel.onopen = async function () {
-			console.log('data channel open', channel.readyState)
+			console.log('data channel open')
+			document.querySelector('form#webrtc button').disabled = false
 		}
 		channel.onclose = async function (event) {
 			console.log('data channel close', event)
@@ -32,15 +50,17 @@ function peerConnection() {
 		channel.onmessage = async function (event) {
 			console.log('data channel message', decode(event.data))
 		}
-	}
-	peer.onmessage = function (event) {
-		console.log(event)
+		channel.onerror = function (event) {
+			console.error(event.error)
+		}
 	}
 	return peer
 }
 
+// Create the peer connection with a data channel.
 const peer = peerConnection()
 const dataChannel = peer.createDataChannel('main')
+
 
 // Form to send message via WebRTC.
 document.querySelector('form#webrtc').addEventListener('submit', (event) => {
@@ -51,23 +71,23 @@ document.querySelector('form#webrtc').addEventListener('submit', (event) => {
 	dataChannel.send(encode({custom: 'yolo', msg}))
 })
 
-document.querySelector('form#webrtc-offer').addEventListener('submit', async (event) => {
+// This is the signaling part, but we do it manually with textarea + copy/paste.
+document.querySelector('form#webrtc-signal').addEventListener('submit', async (event) => {
 	event.preventDefault()
 	const data = new FormData(event.target)
 	const msg = decode(data.get('message'))
-	console.log(msg)
-	if (msg.type === 'offer') {
+	if (msg.sdp) {
+		console.log('accepted remote description')
 		const desc = new RTCSessionDescription(msg)
 		peer.setRemoteDescription(desc)
-		console.log('accepted offer')
-		const answer = await peer.createAnswer()
-		await peer.setLocalDescription(answer)
-		console.log('answer', encode(answer))
-	}
-	if (msg.type === 'answer') {
-		const desc = new RTCSessionDescription(msg)
-		await peer.setRemoteDescription(desc)
-		console.log('accepted answer')
+		if (msg.type === 'offer') {
+			console.log('setting local description with answer')
+			const answer = await peer.createAnswer()
+			await peer.setLocalDescription(answer)
+			console.log('answer', encode(answer))
+		}
+		if (msg.type === 'answer') {
+		}
 	}
 })
 
@@ -82,3 +102,5 @@ document.querySelector('button#connect').addEventListener('click', async (event)
 // Helpers
 const encode = (data) => btoa(JSON.stringify(data))
 const decode = (data) => JSON.parse(atob(data))
+// for debugging
+window.i4k = {peer, encode, decode}
